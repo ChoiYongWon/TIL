be8db33db790210a533f0bdb699f9682f628d093
ChoiYongWon <yongwon0824@naver.com>
2023-02-18 05:53:07 +0900

    feat: change docs dir to lowercase & add: README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# Today I Learned
+
+https://til.choiyongwon.me
+
+@ChoiYongWon 이 오늘 새로 배운 것을 다음의 규칙으로 commit 합니다.
--- docs/.vuepress/const.js
+++ docs/.vuepress/const.js
@@ -1,22 +1,22 @@
 exports.AWS = [
-  "AWS/[EC2] Instance store vs EBS",
-  "AWS/[Global] Edge Location",
-  "AWS/[IAM] user vs role",
-  "AWS/[IAM] role 이란",
-  "AWS/[Lambda] lambda 특징",
-  "AWS/[S3] S3 스토리지 클래스",
-  "AWS/[S3] S3 특징",
-  "AWS/[S3] S3에 파일을 빨리 업로드하는 방법",
-  "AWS/[Security] 다중 방어 계층",
-  "AWS/[VPC] Subnet 예약 IP",
-  "AWS/[VPC] Subnet 특징",
-  "AWS/[VPC] NAT Gateway의 역할",
-  "AWS/[VPC] VPC Peering",
-  "AWS/[VPC] Transit Gateway",
-  "AWS/[VPC] VPC Endpoint",
-  "AWS/[VPC] 가용영역과 리전",
+  "aws/[EC2] Instance store vs EBS",
+  "aws/[Global] Edge Location",
+  "aws/[IAM] user vs role",
+  "aws/[IAM] role 이란",
+  "aws/[Lambda] lambda 특징",
+  "aws/[S3] S3 스토리지 클래스",
+  "aws/[S3] S3 특징",
+  "aws/[S3] S3에 파일을 빨리 업로드하는 방법",
+  "aws/[Security] 다중 방어 계층",
+  "aws/[VPC] Subnet 예약 IP",
+  "aws/[VPC] Subnet 특징",
+  "aws/[VPC] NAT Gateway의 역할",
+  "aws/[VPC] VPC Peering",
+  "aws/[VPC] Transit Gateway",
+  "aws/[VPC] VPC Endpoint",
+  "aws/[VPC] 가용영역과 리전",
 ];
 
-exports.CLOUD = ["Cloud/클라우드 환경은 어떻게 이루어져 있을까"];
+exports.CLOUD = ["cloud/클라우드 환경은 어떻게 이루어져 있을까"];
 
-exports.NETWORK = ["Network/TCP-IP vs OSI 7계층"];
+exports.NETWORK = ["network/TCP-IP vs OSI 7계층"];
