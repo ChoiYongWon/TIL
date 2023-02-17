67ef69576e4ea2aa19bf55f189ce69b3a85e7711
ChoiYongWon <yongwon0824@naver.com>
2023-02-17 12:01:00 +0900

    test: Init

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,6 @@ const CONST = require("./const");
 module.exports = {
   title: "ChoiYongWon's TIL",
   description: "ChoiYongWon의 Today I Learned",
-  dest: "build",
   base: "/TIL/",
   themeConfig: {
     nav: [{ text: "Github", link: "https://github.com/ChoiYongWon" }],
