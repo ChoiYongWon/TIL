fed718a7d9f97c4b64532e391b8ad69e69bffda3
ChoiYongWon <yongwon0824@naver.com>
2023-02-17 12:58:28 +0900

    fix: base url:

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ const CONST = require("./const");
 module.exports = {
   title: "ChoiYongWon's TIL",
   description: "ChoiYongWon의 Today I Learned",
-  base: "/TIL/",
+  // base: "/TIL/",
   themeConfig: {
     nav: [{ text: "Github", link: "https://github.com/ChoiYongWon" }],
     sidebar: [
